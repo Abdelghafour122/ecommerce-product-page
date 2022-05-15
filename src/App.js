0733d0e7f8674e44142a7631794b66e81190e09d
Abdelghafour <ghafourragg@gmail.com>
2022-05-15 19:58:38 +0100

    :

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Container } from "@mui/material";
 import Navbar from "./Components/Navbar";
