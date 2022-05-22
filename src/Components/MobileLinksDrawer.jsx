88263732f9c0b018b2ae025c82586cdfd5aba860
Abdelghafour <ghafourragg@gmail.com>
2022-05-22 12:30:28 +0100

    fixed drawer width

--- src/Components/MobileLinksDrawer.jsx
+++ src/Components/MobileLinksDrawer.jsx
@@ -17,40 +17,39 @@ const MobileLinksDrawer = ({ onOpen, onHandleOpen }) => {
         flexDirection: "column",
         padding: "30px",
         height: "100vh",
-        "& .css-4t3x6l-MuiPaper-root-MuiDrawer-paper": {
-          width: "65vw",
-        },
       }}
     >
-      <section className="closing">
-        <IconButton
-          disableRipple
-          onClick={() => {
-            onHandleOpen(false);
-          }}
-        >
-          <CloseIcon fillColor={"#68707d"} />
-        </IconButton>
-      </section>
-      <section className="mobile-links">
-        <ul>
-          <li>
-            <button>Collections</button>
-          </li>
-          <li>
-            <button>Men</button>
-          </li>
-          <li>
-            <button>Women</button>
-          </li>
-          <li>
-            <button>About</button>
-          </li>
-          <li>
-            <button>Contact</button>
-          </li>
-        </ul>
-      </section>
+      <div className="draw" style={{ width: "65vw" }}>
+        <section className="closing">
+          <IconButton
+            disableRipple
+            onClick={() => {
+              onHandleOpen(false);
+            }}
+          >
+            <CloseIcon fillColor={"#68707d"} />
+          </IconButton>
+        </section>
+        <section className="mobile-links">
+          <ul>
+            <li>
+              <button>Collections</button>
+            </li>
+            <li>
+              <button>Men</button>
+            </li>
+            <li>
+              <button>Women</button>
+            </li>
+            <li>
+              <button>About</button>
+            </li>
+            <li>
+              <button>Contact</button>
+            </li>
+          </ul>
+        </section>
+      </div>
     </Drawer>
   );
 };
